69957bf0c210410bd26b255ad4968012d90e135f
angie <angheloalf95@gmail.com>
2021-11-15 12:32:25 -0300

    ipl3 placeholder

---
+++ asm/usa/makerom/ipl3.s
@@ -0,0 +1,12 @@
+.include "macro.inc"
+
+# assembler directives
+.set noat      # allow manual use of $at
+.set noreorder # don't insert nops after branches
+.set gp=64     # allow use of 64-bit general purpose registers
+
+.section .text
+
+.balign 16
+
+.incbin "baserom/usa/makerom.bin", 0x40, 0xFC0
