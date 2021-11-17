7fb3a539521f51a415ffa02ed4b0512f2881d677
angie <angheloalf95@gmail.com>
2021-11-17 17:24:08 -0300

    Use boot disassembly to build ROM
    with a small workaround in ll

---
+++ asm/usa/workaround_ll.s
@@ -0,0 +1,12 @@
+.include "macro.inc"
+
+# assembler directives
+.set noat      # allow manual use of $at
+.set noreorder # don't insert nops after branches
+.set gp=64     # allow use of 64-bit general purpose registers
+
+.section .text
+
+.balign 16
+
+.incbin "baserom/usa/boot.bin", 0x51F00, 0x2C0
