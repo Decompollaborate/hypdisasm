e9c604b1bf8265d36210ed6a15a5a458d73f975c
angie <angheloalf95@gmail.com>
2021-11-17 13:47:54 -0300

    Don't disassemble functions with swc2 and lwc2

--- tools/automatizators/context_to_undefinedsyms.sh
+++ tools/automatizators/context_to_undefinedsyms.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # This oneliner is kinda bad
-grep -E '^symbol,'  context/usa/boot_.txt | grep -E ',False$' | sed 's/\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\)/\4 = \3;/' | sort --field-separator== -k 2 > undefined_syms_usa_boot.txt
+grep -E '^symbol,'  context/usa/boot_.txt | grep -E ',False$' | sed 's/\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\)/\4 = \3;/' > undefined_syms_usa_boot.txt
+grep -E '^function,' context/usa/boot_.txt | grep -E ',False$' | sed 's/\(.*\),\(.*\),\(.*\),\(.*\),\(.*\),\(.*\)/\4 = \3;/' >> undefined_syms_usa_boot.txt
+grep -E '^fake_function,' context/usa/boot_.txt | sed 's/\(.*\),\(.*\),\(.*\),\(.*\),\(.*\)/\4 = \3;/' >> undefined_syms_usa_boot.txt
+
+# Sort it!
+sort --field-separator== -k 2 undefined_syms_usa_boot.txt -o undefined_syms_usa_boot.txt
